0f2793da37f4bea1269cde1fb11211bd91eb76e1
CarolinaAlbanS <carolina.alban.sandoval@gmail.com>
2024-01-30 17:35:02 +0100

    personajes creados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { GamesContextProvider } from "./Contexto/Contexto";
+import Home from "./componentes/Home/Home";
+import Personajes from "./componentes/Personajes/Personajes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GamesContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/personajes" element={<Personajes />} />
+          </Routes>
+        </BrowserRouter>
+      </GamesContextProvider>
     </div>
   );
 }
