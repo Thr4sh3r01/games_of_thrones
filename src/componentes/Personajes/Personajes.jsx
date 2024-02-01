b867230fd97462ffa7c97998e4c950153161cea4
Thr4sh3r01 <thr4sh3r01@gmail.com>
2024-02-01 01:29:51 +0100

    simplebar y algunas clases y css

--- src/componentes/Personajes/Personajes.jsx
+++ src/componentes/Personajes/Personajes.jsx
@@ -3,6 +3,9 @@ import { GamesContext } from "../../Contexto/Contexto";
 import { Link } from "react-router-dom";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
+import SimpleBar from 'simplebar-react';
+import './Personajes.css';
+
 
 const Personajes = () => {
   const { characters } = useContext(GamesContext);
@@ -11,12 +14,14 @@ const Personajes = () => {
     <div>
       <Header></Header>
       {characters.map((character, index) => (
-        <div key={index}>
-          <img src={character.image} alt={character.name} />
-          <Link to={`/personajes/${character.id}`}>
-            <h2>{character.name}</h2>
-          </Link>
-        </div>
+        <SimpleBar>  
+          <div className="contenedorPersonajes" key={index}>
+            <img className="imagen" src={character.image} alt={character.name} />
+              <Link to={`/personajes/${character.id}`}>
+                <h2>{character.name}</h2>
+              </Link>
+          </div>
+        </SimpleBar>
       ))}
       <Footer></Footer>
     </div>
